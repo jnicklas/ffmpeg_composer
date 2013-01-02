2daa1c8a37f6913c931fc4656a962a134bf5eb08
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2013-01-02 17:41:24 +0100

    Benchmark in memtest

--- examples/memtest.rb
+++ examples/memtest.rb
@@ -1,18 +1,21 @@
 require "ffmpeg_composer"
 require "fileutils"
+require "benchmark"
 
 file = File.expand_path("fixtures/result.mpg", File.dirname(__FILE__))
 frame1 = File.expand_path("fixtures/img0.jpg", File.dirname(__FILE__))
 
-1_000.times do
-  FileUtils.rm(file) if File.exist?(file)
-  composer = FFmpegComposer.new(file, 25)
-  composer.start
-  composer.add_frame(frame1, 10)
-  composer.finish
-  print "."
-  GC.start
+bm = Benchmark.measure do
+  1_000.times do
+    FileUtils.rm(file) if File.exist?(file)
+    composer = FFmpegComposer.new(file, 25)
+    composer.start
+    composer.add_frame(frame1, 10)
+    composer.finish
+    print "."
+  end
 end
 puts "\ndone"
+puts bm
 
 sleep
